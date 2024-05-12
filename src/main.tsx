50464e944c6863e3e50ef0fcfc5e473235d138bf
Jicheng Zhi <jzhi0004@student.monash.edu>
2024-05-13 00:43:22 +1000

    feat: add navbar and authentication

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 // Import the generated route tree
+import { Auth0Provider } from '@auth0/auth0-react';
 import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
@@ -19,17 +20,17 @@ declare module '@tanstack/react-router' {
 
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
-    {/*<Auth0Provider*/}
-    {/*  domain={import.meta.env.VITE_AUTH0_DOMAIN}*/}
-    {/*  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}*/}
-    {/*  authorizationParams={{*/}
-    {/*    redirect_uri: window.location.origin,*/}
-    {/*    audience: import.meta.env.VITE_AUTH0_AUDIENCE,*/}
-    {/*  }}*/}
-    {/*>*/}
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
-    {/*</Auth0Provider>*/}
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      }}
+    >
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Auth0Provider>
   </React.StrictMode>,
 );
